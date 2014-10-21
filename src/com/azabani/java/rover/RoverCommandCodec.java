1cebd560b2feb037091914131e501e3de659456d
Delan Azabani <delan@azabani.com>
2014-10-21 16:33:02 +0800

    Java: RoverCommandCodec.decode() now decodes lists of tasks

--- src/com/azabani/java/rover/RoverCommandCodec.java
+++ src/com/azabani/java/rover/RoverCommandCodec.java
@@ -4,26 +4,31 @@ public class RoverCommandCodec {
 	public String encode(RoverCommand c) {
 		return c.toString();
 	}
-	public RoverCommand decode(String s) {
-		String[] tokens = s.split(":", 2);
-		String kind = tokens[0];
-		if (kind.equals("rotate")) {
-			double angle = Double.parseDouble(tokens[1]);
-			return new RotationCommand(angle);
-		} else if (kind.equals("translate")) {
-			double distance = Double.parseDouble(tokens[1]);
-			return new TranslationCommand(distance);
-		} else if (kind.equals("analyse")) {
-			return new AnalysisCommand();
-		} else if (kind.equals("photograph")) {
-			return new PhotographyCommand();
-		} else if (kind.equals("call")) {
-			int listIndex = Integer.parseInt(tokens[1]);
-			return new RecursionCommand(listIndex);
-		} else {
-			throw new IllegalArgumentException(
-				"invalid command string"
-			);
+	public RoverTaskList decode(String s) {
+		RoverTaskList result = new RoverTaskList();
+		String[] tasks = s.split(";");
+		for (String task : tasks) {
+			String[] tokens = s.split(":", 2);
+			String kind = tokens[0];
+			if (kind.equals("rotate")) {
+				double angle = Double.parseDouble(tokens[1]);
+				result.add(new RotationCommand(angle));
+			} else if (kind.equals("translate")) {
+				double dist = Double.parseDouble(tokens[1]);
+				result.add(new TranslationCommand(dist));
+			} else if (kind.equals("analyse")) {
+				result.add(new AnalysisCommand());
+			} else if (kind.equals("photograph")) {
+				result.add(new PhotographyCommand());
+			} else if (kind.equals("call")) {
+				int listIndex = Integer.parseInt(tokens[1]);
+				result.add(new RecursionCommand(listIndex));
+			} else {
+				throw new IllegalArgumentException(
+					"invalid command string"
+				);
+			}
 		}
+		return result;
 	}
 }
