34dce02814b75cd0195a155dac1bb0a9386deb08
Delan Azabani <delan@azabani.com>
2014-10-21 13:43:13 +0800

    Java: create RoverCommandCodec (incomplete)

---
+++ src/com/azabani/java/rover/RoverCommandCodec.java
@@ -0,0 +1,12 @@
+package com.azabani.java.rover;
+
+public class RoverCommandCodec {
+	public static String encode(RoverCommand c) {
+		// TODO: write the encoding algorithm
+		return "";
+	}
+	public static RoverCommand decode(String s) {
+		// TODO: write the decoding algorithm
+		return null;
+	}
+}
