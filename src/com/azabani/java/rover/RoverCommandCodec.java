10f7c325bf073dbf3c4cbb9f3da4cff1d1b21c04
Delan Azabani <delan@azabani.com>
2014-10-21 14:35:14 +0800

    Java: implement RoverCommandCodec.decode()

--- src/com/azabani/java/rover/RoverCommandCodec.java
+++ src/com/azabani/java/rover/RoverCommandCodec.java
@@ -5,7 +5,25 @@ public class RoverCommandCodec {
 		return c.toString();
 	}
 	public static RoverCommand decode(String s) {
-		// TODO: write the decoding algorithm
-		return null;
+		String[] tokens = s.split(":", 2);
+		String kind = tokens[0];
+		if (kind.equals("rotate")) {
+			double angle = Double.parseDouble(tokens[1]);
+			return new RotationCommand(angle);
+		} else if (kind.equals("translate")) {
+			double distance = Double.parseDouble(tokens[1]);
+			return new TranslationCommand(distance);
+		} else if (kind.equals("analyse")) {
+			return new AnalysisCommand();
+		} else if (kind.equals("photograph")) {
+			return new PhotographyCommand();
+		} else if (kind.equals("call")) {
+			int listIndex = Integer.parseInt(tokens[1]);
+			return new RecursionCommand(listIndex);
+		} else {
+			throw new IllegalArgumentException(
+				"invalid command string"
+			);
+		}
 	}
 }
