bd6b86b9ec4b88ef152f7e0c26a2c2fc1f6492ae
Delan Azabani <delan@azabani.com>
2014-10-21 15:51:42 +0800

    Java: implement ConcreteCamera (untested)

---
+++ src/com/azabani/java/rover/ConcreteCamera.java
@@ -0,0 +1,26 @@
+package com.azabani.java.rover;
+
+import com.azabani.java.rover.fake.Camera;
+import java.util.List;
+import java.util.LinkedList;
+
+public class ConcreteCamera extends Camera
+	implements ObservableDevice {
+	private List<DeviceObserver> observers;
+	public ConcreteCamera() {
+		observers = new LinkedList<DeviceObserver>();
+	}
+	protected void photoReady(char[] photoData) {
+		notifyObservers(photoData);
+	}
+	public void addObserver(DeviceObserver o) {
+		observers.add(o);
+	}
+	public void deleteObserver(DeviceObserver o) {
+		observers.remove(o);
+	}
+	public void notifyObservers(Object x) {
+		for (DeviceObserver o : observers)
+			o.update(x);
+	}
+}
