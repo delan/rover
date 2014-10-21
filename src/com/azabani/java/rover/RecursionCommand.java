297e902f43843bfa3c5c8d5d3ad44f9992ff4963
Delan Azabani <delan@azabani.com>
2014-10-21 14:21:42 +0800

    Java: allow users of RecursionCommand to set program/stack late

--- src/com/azabani/java/rover/RecursionCommand.java
+++ src/com/azabani/java/rover/RecursionCommand.java
@@ -7,15 +7,18 @@ public class RecursionCommand implements RoverCommand {
 	int listIndex; // non-negative
 	RoverListList program;
 	Stack<Iterator<RoverCommand>> stack;
-	public RecursionCommand(int listIndex, RoverListList program,
-		Stack<Iterator<RoverCommand>> stack)
+	public RecursionCommand(int listIndex)
 		throws IllegalArgumentException {
 		if (listIndex < 0)
 			throw new IllegalArgumentException(
 				"listIndex must not be negative"
 			);
 		this.listIndex = listIndex;
+	}
+	public void setProgram(RoverListList program) {
 		this.program = program;
+	}
+	public void setStack(Stack<Iterator<RoverCommand>> stack) {
 		this.stack = stack;
 	}
 	public void execute() {
